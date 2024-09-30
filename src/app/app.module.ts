75bfcc19fd511401d1de9c9b87e86c39a5534e94
Juuhey <jujupantel@gmail.com>
2024-09-30 11:01:29 +0200

    FEAT. Transloco (clean install)
    Ajout de transloco, il fonctionne bien avec les assets (sans le dossier public)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { provideHttpClient } from "@angular/common/http";
+import { provideHttpClient, HttpClientModule } from "@angular/common/http";
 import { PokemonListComponent } from './pages/pokemon-list/pokemon-list.component';
 import { PokemonCardComponent } from './shared/layouts/pokemon-card/pokemon-card.component';
 import { PokemonDetailComponent } from './pages/pokemon-detail/pokemon-detail.component';
-import { FormatPokemonIdPipe } from './format-pokemon-id.pipe'; // Import du provider
+import { FormatPokemonIdPipe } from './format-pokemon-id.pipe';
+import { TranslocoRootModule } from './transloco-root.module';
+import { LangSelectorComponent } from './shared/layouts/lang-selector/lang-selector.component'; // Import du provider
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,11 +18,15 @@ import { FormatPokemonIdPipe } from './format-pokemon-id.pipe'; // Import du pro
     PokemonListComponent,
     PokemonCardComponent,
     PokemonDetailComponent,
-    FormatPokemonIdPipe
+    FormatPokemonIdPipe,
+    LangSelectorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslocoRootModule,
+    FormsModule
   ],
   providers: [
     provideHttpClient(), // Ajout du provider
